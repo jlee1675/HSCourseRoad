114b2a2d65ed08a1d255cde4e9dd5c4c00da49b7
Jessica Lee <Jennifer@Jessicas-MacBook-Pro.local>
2018-03-25 16:10:23 -0700

    bootstrapped version

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import Header from './containers/Header';
 import List from './containers/List';
 import Footer from './containers/Footer';
+import Menu from './components/Menu'
 //import LeftMenu from './containers/LeftMenu';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className = "Header"> Course Road </div>
+      <div className="container">
+        <Menu />
         <List className= "List" /> 
         <div className = "LeftMenu"></div>
       </div>
