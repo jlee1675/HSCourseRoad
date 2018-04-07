8ec409cc5a383276d4fe0f6169a349d6054b0bf1
Jessica Lee <Jennifer@Jessicas-MacBook-Pro.local>
2018-04-07 11:31:04 -0700

    added course button

--- src/containers/LeftMenu.js
+++ src/containers/LeftMenu.js
@@ -34,6 +34,22 @@ class LeftMenu extends Component {
                         <a className="dropdown-item" href="#">Senior</a>
                     </div>
                 </div>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                       Year
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a className="dropdown-item" href="#">English</a>
+                        <a className="dropdown-item" href="#">Mathematics</a>
+                        <a className="dropdown-item" href="#">Physical Education</a>
+                        <a className="dropdown-item" href="#">Science</a>
+                        <a className="dropdown-item" href="#">Social Science</a>
+                        <a className="dropdown-item" href="#">Visual and Performing Arts</a>
+                        <a className="dropdown-item" href="#">World Language</a>
+
+
+                    </div>
+                </div>
                 {
                     this.state.courses.map((d,i) => (
                         <button key={i} className="btn btn-primary" type="submit" onClick={() => {this.props.add}}>{d.name}</button>
